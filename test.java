489a81ecedf1b9b9173a1fc400ccf3bbca82f466
ensign-redshirt <alewis3@mail.umw.edu>
2013-09-04 12:28:50 -0400

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,20 @@
 Annika:
 540 222 0135
 alewis3@mail.umw.edu
+Schedule:
+Mondays: only 330.
+Tues: 10-1: work at help desk
+      2-315: 420
+      6-930: GIS 351
+Wed: 330.
+      1-3: work at help desk
+Thurs: 12 - 2: work at help desk
+      2-315: 420
+      6-9:30: GIS 471
+Fri: 12-3: work at help desk      
+      
+      
+
 
 Riley:
 703 473 5684
