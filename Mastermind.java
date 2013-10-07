1d57ee565057aa756a2302e6be71da9aecd99e92
ensign-redshirt <alewis3@mail.umw.edu>
2013-10-07 12:08:42 -0400

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -18,6 +18,7 @@ public class Mastermind {
 	do{
 	    System.out.println("Enter your guess:");
 	    string g = in.next();
+	    g.toUpperCase();
 	    LetterCheck(num, g);
 	    char guess[] = g.toCharArray;
 	    guessVarify(guess[], password[]);
