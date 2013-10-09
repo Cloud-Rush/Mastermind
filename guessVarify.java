73c95e6d661e9f5cfaa8f9893cb9965b66b7e716
ensign-redshirt <alewis3@mail.umw.edu>
2013-10-08 21:54:15 -0400

    Update guessVarify.java

--- guessVarify.java
+++ guessVarify.java
@@ -4,50 +4,49 @@ public class guessVarify{
 
 static Boolean guessVarify(char[] guess, char[] pass)
 {
-	int guessLength = guess.length;
-	int passLength = pass.length;
-	char[] guessHolder = new char[4];
-	char [] passHolder = new char[4];
-	for(int i=0;i<4;i++){
-		guessHolder[i] = guess[i];
-		passHolder[i] = pass[i];
-	}
-	int exactright=0;
-	int right=0;
-	String guess2 = new String (guess);
-	String pass2 = new String (pass);
-	System.out.println("guess2:" + guess2);
-	System.out.println("pass2:" + pass2);
-	
+public class guessVarify{
 
 
-	for (int i=0; i < passLength; i++){
-		
-		//see if i letter in pass is exactly right with i letter in guess
-		if (passHolder[i] == (guessHolder[i])){
-			exactright = exactright + 1;
-			passHolder[i] = '.';
-			guessHolder[i] = ',';
-		}
-		
-	}
-	
-	//runs through the length of the password
-	for (int i=0; i < passLength; i++){
-		
-		//see if i letter in pass is found somewhere in guess
-		for (int x = 0; x < passLength; x++){
-			if (passHolder[i] == guessHolder[x]) {
-				right = right +1;
-				passHolder[i] = '.';
-				guessHolder[x] = ',';
-				break;
-			}
-		}
-		
-	}
-	
-	
+static Boolean guessVarify(char[] guess, char[] pass)
+{
+        int guessLength = guess.length;
+        int passLength = pass.length;
+        int exactright=0;
+        int right=0;
+        String g = new String (guess);
+        String p = new String (pass);
+        char[] a = g.toCharArray();
+        char[] b = p.toCharArray();
+
+
+        for (int i=0; i < passLength; i++){
+
+                //see if i letter in pass is exactly right with i letter in gues
+s
+                if (b[i] == (a[i])){
+                        exactright = exactright + 1;
+                        b[i] = '.';
+                        a[i] = ',';
+
+                }
+
+        }
+
+        //runs through the length of the password
+        for (int r=0; r < passLength; r++){
+
+                //see if r letter in pass is found somewhere in guess
+                for (int x = 0; x < passLength; x++){
+                        if (b[r] == a[x]) {
+                                right = right +1;
+                                b[r] = '.';
+                                a[x] = ',';
+                                break;
+                        }
+
+                }
+        }
+
 	if (exactright == passLength){
 		return true;
 	}
