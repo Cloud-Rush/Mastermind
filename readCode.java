eee21ff48f80cc194fc1c9a70cf6a11d57ea24ab
ensign-redshirt <alewis3@mail.umw.edu>
2013-10-08 21:51:21 -0400

    Update readCode.java

--- readCode.java
+++ readCode.java
@@ -3,50 +3,37 @@ import java.util.*;
 import java.util.Scanner;
 
 public class readCode {
-	
  public static String readCode(int num) {
-	Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
     Boolean correct = false;
     Boolean length = false;
     String readPassword;
     Boolean letters ;
-    
+
     do{
-	      System.out.println("Enter code:");
-	      readPassword = in.nextLine();
-	      int lengthOfPass = readPassword.length();
-	     if (lengthOfPass < num || lengthOfPass > num){
-	    	 System.out.println("please enter a valid number for your code");
-	    	 correct = false;
-	    	 length = false;
-	    	 continue;
-	     }
-	      
-	      letters = isAlpha(readPassword);
-    	if (letters == false){
-    		correct = false;
-    		continue;
-    	}
-    	else{
-    		letters = true;
-    		correct = true;
-    	}
-    } while(correct == false && length == false );
-    
-    return (readPassword);
-    
-  }
+              System.out.println("Enter code and press enter: ");
+              readPassword = Pass.Pass();
+              int lengthOfPass = readPassword.length();
+             if (lengthOfPass < num || lengthOfPass > num){
+                 System.out.println("please enter a valid number for your code")
+;
+                 correct = false;
+                 length = false;
+                 continue;
+             }
 
-public static boolean isAlpha(String name) {
-    char[] chars = name.toCharArray();
-
-    for (char c : chars) {
-        if(!Character.isLetter(c)) {
-            return false;
+              letters = isAlpha.isAlpha(readPassword);
+        if (letters == false){
+                correct = false;
+                continue;
         }
-    }
+        else{
+                letters = true;
+                correct = true;
+        }
+    } while(correct == false && length == false );
 
-    return true;
-}
-}
+    return (readPassword);
 
+  }
+}
