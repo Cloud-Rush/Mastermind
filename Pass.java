9ed7da97d122d3df3de6e4b5d13801cbccfd90be
ensign-redshirt <alewis3@mail.umw.edu>
2013-10-08 21:49:53 -0400

    masks password

---
+++ Pass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.Console;
+
+public class Pass {
+    public static String Pass(){
+    Console console = System.console( );
+    return new String(console.readPassword( ));
+  }
+}
