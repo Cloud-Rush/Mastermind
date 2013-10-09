d7e7a9bb1611e145006444b5995e746427f7b14c
ensign-redshirt <alewis3@mail.umw.edu>
2013-10-08 21:50:34 -0400

    checks if it's alphabet char

---
+++ isAlpha.java
@@ -0,0 +1,13 @@
+public class isAlpha{
+public static boolean isAlpha(String name) {
+    char[] chars = name.toCharArray();
+
+    for (char c : chars) {
+        if(!Character.isLetter(c)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+}
